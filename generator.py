62ae767cd2e54f8c87995f182eac82b4e18b566c
Tony Houghton <h@realh.co.uk>
2022-12-28 16:19:33 +0000

    Moved docstrings to after their definitions.
    Also avoid extra blank lines after empty enum/message definitions.

--- generator.py
+++ generator.py
@@ -10,12 +10,12 @@ from google.protobuf.descriptor_pb2 import \
 
 import log
 
-''' A Generator loads a .proto file and generates some source code from it.
-    It must be overridden to provide ?? methods. '''
 class Generator:
-    ''' baseName is the base name of the plugin eg protoc-gen-ktdata has a
-        base name of "ktdata". '''
+    ''' A Generator loads a .proto file and generates some source code from it.
+        It must be overridden to provide ?? methods. '''
     def __init__(self, baseName: str):
+        ''' baseName is the base name of the plugin eg protoc-gen-ktdata has a
+            base name of "ktdata". '''
         self.baseName = baseName
         self.log = log.getLogger("protoc-gen-" + baseName)
         self.log.debug("sys.argv = %s" % str(sys.argv))
@@ -48,28 +48,31 @@ class Generator:
             "bytes": "ByteArray"
         }
 
-    ''' This can be used as the entry point to load the CodeGeneratorRequest
-        from stdin, process it and write the CodeGeneratorResponse to stdout.
-    '''
     def runOnStdinAndStdout(self):
+        ''' This can be used as the entry point to load the
+            CodeGeneratorRequest from stdin, process it and write the
+            CodeGeneratorResponse to stdout.
+        '''
         input = sys.stdin.buffer.read()
         req = CodeGeneratorRequest.FromString(input)
         response = self.process(req)
         output = response.SerializeToString()
         sys.stdout.buffer.write(output)
     
-    ''' Processes a CodeGeneratorRequest and generates a CodeGeneratorResponse
-        by calling self.processFile() for each proto file in req. '''
     def process(self, req: CodeGeneratorRequest) -> CodeGeneratorResponse:
+        ''' Processes a CodeGeneratorRequest and generates a
+            CodeGeneratorResponse by calling self.processFile() for each proto
+            file in req. '''
         response = CodeGeneratorResponse()
         for f in req.proto_file:
             self.processFile(f, response)
         return response
     
-    ''' Processes each proto file, adding a new output file to the response.
-        It sets self.options to a dict of the options read from protoFile. '''
     def processFile(self, protoFile: FileDescriptorProto,
                     response: CodeGeneratorResponse):
+        ''' Processes each proto file, adding a new output file to the response.
+            It sets self.options to a dict of the options read from protoFile.
+        '''
         self.options = self.getOptions(protoFile)
         fileName = self.getOutputFileName(protoFile.name,
                 protoFile.package)
@@ -77,57 +80,62 @@ class Generator:
         file.name = fileName
         file.content = self.getContent(protoFile)
     
-    ''' Returns a dict of options read from protoFile. '''
     def getOptions(self, protoFile: FileDescriptorProto) -> dict[str, str]:
+        ''' Returns a dict of options read from protoFile. '''
         lines = str(protoFile.options).strip().split("\n")
         options = (l.split(": ") for l in lines)
         kvs = ((i[0], i[1].replace('"', "")) for i in options)
         optsDict = dict(i for i in kvs)
         return optsDict
 
-    ''' Gets an output file name based on the input proto filename and its
-        package name. '''
     def getOutputFileName(self, protoFileName: str, packageName: str) -> str:
+        ''' Gets an output file name based on the input proto filename and its
+            package name. '''
         return protoFileName + ".kt"
 
-    ''' Generates the content for an output file as a string. '''
     def getContent(self, protoFile: FileDescriptorProto) -> str:
+        ''' Generates the content for an output file as a string. '''
         lines = self.getHeader(protoFile)
         indentationLevel = self.getTopLevelIndentation(protoFile)
         for enum in protoFile.enum_type:
-            lines.extend(self.processEnum(enum, indentationLevel))
-            lines.append("")
+            e = self.processEnum(enum, indentationLevel)
+            if len(e) > 0:
+                lines.extend(e)
+                lines.append("")
         for msg in protoFile.message_type:
-            lines.extend(self.processMessage(msg, indentationLevel))
-            lines.append("")
+            m = self.processMessage(msg, indentationLevel)
+            if len(m) > 0:
+                lines.extend(m)
+                lines.append("")
         lines.extend(self.getFooter(protoFile))
         return "\n".join(lines) + "\n"
     
-    ''' Gets the first few lines to start the output file. '''
     def getHeader(self, protoFile: FileDescriptorProto) -> list[str]:
+        ''' Gets the first few lines to start the output file. '''
         return []
     
-    ''' Gets the last few lines to add to the end of the output file. '''
     def getFooter(self, protoFile: FileDescriptorProto) -> list[str]:
+        ''' Gets the last few lines to add to the end of the output file. '''
         return []
     
-    ''' Gets the indentation to use for the top-level classes. '''
     def getTopLevelIndentation(self, protoFile: FileDescriptorProto) -> int:
+        ''' Gets the indentation to use for the top-level classes. '''
         return 0
     
-    ''' Returns a list of strings (one per line) for an enum definition.
-        Each line is indented by an additional number of spaces multiplied by
-        indentationLevel. '''
     def processEnum(self, enum: EnumDescriptorProto,
                     indentationLevel: int) -> list[str]:
+        ''' Returns a list of strings (one per line) for an enum definition.
+            Each line is indented by an additional number of spaces multiplied
+            by indentationLevel. '''
         raise NotImplementedError("processEnum not overridden in %s",
                 self.baseName)
     
-    ''' Returns a representation of a protobuf message by calling
-        messageOpening(), then messageField() repeatedly, then messageClosing().
-    '''
     def processMessage(self, msg: DescriptorProto,
                        indentationLevel: int) -> list[str]:
+        ''' Returns a representation of a protobuf message by calling
+            messageOpening(), then messageField() repeatedly, then
+            messageClosing(). Each line is indented by an additional number of
+            spaces multiplied by indentationLevel. '''
         name = self.typeNameCase(msg.name)
         lines = self.messageOpening(msg, name, indentationLevel)
         indentationLevel += 1
@@ -143,15 +151,15 @@ class Generator:
         lines.extend(self.messageClosing(msg, name, indentationLevel))
         return lines
     
-    ''' Processes a field of a message. '''
     def processField(self, msg: DescriptorProto,
                      field: FieldDescriptorProto,
                      indentationLevel: int) -> list[str]:
+        ''' Processes a field of a message. '''
         raise NotImplementedError("processField not overridden in %s",
                 self.baseName)
 
-    ''' Gets the typename in the target language (here, Kotlin). '''
     def getTypeName(self, number: int, typeName: str | None) -> str:
+        ''' Gets the typename in the target language (here, Kotlin). '''
         if number == FieldDescriptorProto.TYPE_MESSAGE:
             return self.convertTypeName(typeName) + "?"
         elif number == FieldDescriptorProto.TYPE_ENUM:
@@ -167,60 +175,60 @@ class Generator:
             return n
         return self.convertTypeName(typeName) + "?"
     
-    ''' Strips any leading qualifier (includes aren't currently supported).
-        and applies self.typeNameCase. '''
     def convertTypeName(self, name: str) -> str:
+        ''' Strips any leading qualifier (includes aren't currently supported).
+            and applies self.typeNameCase. '''
         if name.startswith("."):
             name = re.sub(r"^\.[a-zA-Z0-9_]*\.", "", name)
         return self.typeNameCase(name)
 
-    ''' Looks up a built-in type by its FieldDescriptorProto.Type value,
-        using self.knownTypes, which can be replaced in a sub-class'
-        constructor if not Kotlin. '''
     def getBuiltInTypeByNumber(self, number: int) -> str | None:
+        ''' Looks up a built-in type by its FieldDescriptorProto.Type value,
+            using self.knownTypes, which can be replaced in a sub-class'
+            constructor if not Kotlin. '''
         if number >= len(self.knownTypes):
             return None
         return self.knownTypes[number]
     
-    ''' Looks up a built-in type by its FieldDescriptorProto.type_name,
-        using self.knownTypesByName, which can be replaced in a sub-class'
-        constructor if not Kotlin. '''
     def getBuiltInTypeByName(self, name: str) -> str | None:
+        ''' Looks up a built-in type by its FieldDescriptorProto.type_name,
+            using self.knownTypesByName, which can be replaced in a sub-class'
+            constructor if not Kotlin. '''
         return self.knownTypesByName(name)
     
-    ''' Returns the opening line(s) of a class etc representing a protobuf
-        message. '''
     def messageOpening(self, msg: DescriptorProto,
                        name: str,
                        indentationLevel: int) -> list[str]:
+        ''' Returns the opening line(s) of a class etc representing a protobuf
+            message. '''
         raise NotImplementedError("messageOpening not overridden in %s",
                 self.baseName)
 
-    ''' Returns the closing line(s) of a class etc representing a protobuf,
-        eg ')' for a Kotlin data class. '''
     def messageClosing(self, msg: DescriptorProto,
                        name: str,
                        indentationLevel: int) -> list[str]:
+        ''' Returns the closing line(s) of a class etc representing a protobuf,
+            eg ')' for a Kotlin data class. '''
         raise NotImplementedError("messageClosing not overridden in %s",
                 self.baseName)
 
-    ''' Converts the case of a name to the appropriate convention for a type
-        name, here "foo_bar" -> "FooBar". '''
     def typeNameCase(self, name: str) -> str:
+        ''' Converts the case of a name to the appropriate convention for a type
+            name, here "foo_bar" -> "FooBar". '''
         if "_" not in name:
             return name[0].upper() + name[1:]
         elements = name.split("_")
         elements = list(e.capitalize() for e in elements)
         return "".join(elements)
 
-    ''' Converts the case of a name to the appropriate convention for an enum
-        member name, here "foo_bar" -> "FOO_BAR". '''
     def enumCase(self, name: str) -> str:
+        ''' Converts the case of a name to the appropriate convention for an
+            enum member name, here "foo_bar" -> "FOO_BAR". '''
         return name.upper()
 
-    ''' Converts the case of a name to the appropriate convention for a method
-        or message field, here camelCase ("foo_bar" -> "fooBar"). '''
     def memberCase(self, name: str) -> str:
+        ''' Converts the case of a name to the appropriate convention for a
+            method or message field, here camelCase ("foo_bar" -> "fooBar"). '''
         if "_" not in name:
             return name[0].lower() + name[1:]
         elements = name.split("_")
@@ -228,8 +236,8 @@ class Generator:
         elements[0] = elements[0][0].lower() + elements[0][1:]
         return "".join(elements)
 
-    ''' The Kotlin message and enum classes should be members of an object
-        to provide them with a namespace to help avoid clashes when accessed
-        from Swift. This returns a suitable name for it. '''
     def getNamespace(self, protoFile: FileDescriptorProto) -> str:
+        ''' The Kotlin message and enum classes should be members of an object
+            to provide them with a namespace to help avoid clashes when accessed
+            from Swift. This returns a suitable name for it. '''
         return self.typeNameCase(protoFile.package) + "ProtoData"
